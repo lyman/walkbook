96f1e5fa41e404b2bad1fc6f98461140bde26ddb
lymanrb <lymanrb@19220d58-0c27-0410-bd10-bb1c23295e66>
2007-01-15 15:32:15 +0000

    <lyman> import existing code into svn  - env: eclipse 3.2 + eclipseme 1.6.1, wtk 2.2

---
+++ src/asiabird/walkbook/ListBookShelf.java
@@ -0,0 +1,49 @@
+﻿/*
+ * Created on 2005-5-31
+ *
+ */
+package asiabird.walkbook;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.microedition.lcdui.Choice;
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.List;
+
+/**
+ * @author Lyman
+ *
+ */
+public class ListBookShelf extends List {
+
+	private Image book = null;
+	private Image how = null;
+
+	public ListBookShelf() {
+		super("WalkBook本地书架", Choice.IMPLICIT);
+
+ 		try	{
+			book = Image.createImage("/image/book.png");
+			how = Image.createImage("/image/how.png");
+ 		} catch (IOException e) {
+			System.out.println("加载 book.png 失败");
+ 		}
+
+		for (int i = 1; i < Config.MAX_BOOK + 1; i++) {
+			InputStream is = null;
+			try {
+				is = getClass().getResourceAsStream("/book/" + i + ".txt");
+				is.read();
+				this.append(i + ".txt", book);
+			} catch(Exception e) {
+				continue;
+			}
+		}
+		this.append("使用说明", how);
+	}
+
+	public String getSelectedItem() {
+		return this.getString(this.getSelectedIndex());
+	}
+}
